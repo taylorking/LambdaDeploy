c9b228a422621ef188b7eedc65acdbc8621a567b
Taylor King <kingtb@appstate.edu>
2015-11-27 16:44:29 -0500

    code cleanup

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ var fs = require('fs');
 var aws = require('aws-sdk');
 var FileReader = require('filereader');
 var lambda = new aws.Lambda();
+
 exports.handler = function(event, context) {
   console.log(event);
   var fileHandle = event.body.repository.url;
   fileHandle = fileHandle.replace(/\.git/g,"");
   fileHandle += "/archive/master.zip"
-  console.log(fileHandle);
+
   var arn = event.arn;
+  
   exec("rm -rf /tmp/*", function(err, stdout, stderr) {
-  var stream = request(fileHandle).pipe(fs.createWriteStream('/tmp/update.zip'));
-  stream.on('finish', function() {
-    exec("./unzip /tmp/update.zip -d /tmp", function(err, stdout, stderr) {
-      exec("cp zip /tmp/*/ && cd /tmp/*/ && ./zip -r /tmp/updateform.zip *", function(err,stdout, stderr) {
-        console.log("stdout on zip: " + stdout);
-        console.log("stderr on zip: " + stderr);
-        //exec("ls /tmp", function(err,stdout, stderr) {
+    var stream = request(fileHandle).pipe(fs.createWriteStream('/tmp/update.zip'));
+    stream.on('finish', function() {
+      exec("./unzip /tmp/update.zip -d /tmp", function(err, stdout, stderr) {
+        exec("cp zip /tmp/*/ && cd /tmp/*/ && ./zip -r /tmp/updateform.zip *", function(err,stdout, stderr) {
+          
           var reader = new FileReader();
       
           var zip = fs.readFileSync("/tmp/updateform.zip");
@@ -34,9 +34,9 @@ exports.handler = function(event, context) {
             }
             context.succeed(data);
           });
+        });
       });
     });
-  });
   });
   //  exec("ls /tmp", function(error, stdout, stderr) {
   //  console.log(stdout);
